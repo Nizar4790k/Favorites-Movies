856c1795a9c981f26f9113b63f7481a479b89438
Nizar <nizar4790k@gmail.com>
2020-10-04 11:01:40 -0400

    remove function finished

--- script.js
+++ script.js
@@ -5,18 +5,27 @@ var movieList = document.getElementById("movieList");
 
 function addMovie(){
 
-    var text = textArea.value;
+    var input = textArea.value;
     var li = document.createElement("li");
     
     var deleteButton = document.createElement("p");
     deleteButton.classList.add("deleteButton");
     deleteButton.appendChild(document.createTextNode("X"));
-    
-    li.appendChild(document.createTextNode(text));
+
+    deleteButton.addEventListener("click",function(ev){
+      var deleteButton=ev.target
+      var li = deleteButton.parentElement;
+      movieList.removeChild(li);      
+    })
+
+
+    li.appendChild(document.createTextNode(input));
     li.appendChild(deleteButton);
     movieList.appendChild(li);
+    textArea.value="";
 
 }
+
 
 addButton.addEventListener("click",addMovie);
 
