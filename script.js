07246c0d8bbae7f93a4177a42e5575aa80003370
Nizar <nizar4790k@gmail.com>
2020-10-06 19:10:45 -0400

    local storage not working

--- script.js
+++ script.js
@@ -3,29 +3,80 @@ var addButton = document.getElementById("addButton");
 var movieList = document.getElementById("movieList");
 
 
-function addMovie(){
 
-    var input = textArea.value;
-    var li = document.createElement("li");
-    
-    var deleteButton = document.createElement("p");
-    deleteButton.classList.add("deleteButton");
-    deleteButton.appendChild(document.createTextNode("X"));
+addButton.addEventListener("click", (ev) => {
 
-    deleteButton.addEventListener("click",function(ev){
-      var deleteButton=ev.target
-      var li = deleteButton.parentElement;
-      movieList.removeChild(li);      
-    })
+  addMovie(textArea.value);
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  var size = localStorage.length;
+
+  for (var i = 1; i < size; i++) {
+
+    addMovie(localStorage.getItem("movie" + i));
+
+  }
+
+});
 
 
-    li.appendChild(document.createTextNode(input));
-    li.appendChild(deleteButton);
-    movieList.appendChild(li);
-    textArea.value="";
+
+function addMovie(input) {
+
+  var li = document.createElement("li");
+
+  var deleteButton = document.createElement("p");
+  deleteButton.classList.add("deleteButton");
+  deleteButton.appendChild(document.createTextNode("X"));
+
+  deleteButton.addEventListener("click", deleteMovie);
+
+  li.appendChild(document.createTextNode(input));
+  li.appendChild(deleteButton);
+  movieList.appendChild(li);
+  textArea.value = "";
+
+
+  saveToLocalStorage();
+
+}
+
+function saveToLocalStorage() {
+
+  localStorage.clear();
+  var moviesLength = movieList.childNodes.length;
+
+  for (var i = 0; i < moviesLength; i++) {
+
+    var movie = movieList.childNodes[i].childNodes[0];
+
+    var key = "movie" + i;
+
+    localStorage.setItem(key, movie);
+
+
+
+  }
+
+  console.log(localStorage);
+
+
+
 
 }
 
 
-addButton.addEventListener("click",addMovie);
+function deleteMovie(ev) {
+  var deleteButton = ev.target
+  var li = deleteButton.parentElement;
+  movieList.removeChild(li);
+
+  saveToLocalStorage();
+}
+
+
+
 
