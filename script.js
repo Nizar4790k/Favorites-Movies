7fa0b20c0357beb82dee5971ed936634aa9e8e16
Nizar <nizar4790k@gmail.com>
2020-10-07 21:18:36 -0400

    add and remove on localstorage finished

--- script.js
+++ script.js
@@ -4,77 +4,124 @@ var movieList = document.getElementById("movieList");
 
 
 
+class Movie{
+
+
+
+  createMovie(name){
+    this.id=createUUID();
+    this.name=name;
+  }
+
+
+
+  createMovieFromInfo(id,name){
+    this.id=id;
+    this.name=name;
+  }
+
+  
+}
+
+
 addButton.addEventListener("click", (ev) => {
 
-  addMovie(textArea.value);
+  var movie = new Movie();
+  movie.createMovie(textArea.value);
+
+
+  localStorage.setItem(movie.id,movie.name);
+  
+  addMovie(movie);
+
+  
+  
 
 });
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
-  var size = localStorage.length;
+  reloadMovies();
 
-  for (var i = 1; i < size; i++) {
-
-    addMovie(localStorage.getItem("movie" + i));
-
-  }
 
 });
 
 
 
-function addMovie(input) {
 
-  var li = document.createElement("li");
 
-  var deleteButton = document.createElement("p");
+
+function addMovie(movie) {
+
+
+
+  const li = document.createElement("li");
+  const deleteButton = document.createElement("a");
   deleteButton.classList.add("deleteButton");
   deleteButton.appendChild(document.createTextNode("X"));
 
-  deleteButton.addEventListener("click", deleteMovie);
+ 
 
-  li.appendChild(document.createTextNode(input));
+  deleteButton.addEventListener("click",function (ev){
+    
+
+    var deleteButton = ev.target
+    var li = deleteButton.parentElement;
+    
+    movieList.removeChild(li);
+    localStorage.removeItem(movie.id);
+  
+    
+  });
+
+
+  
+
+
+
+
+
+  li.appendChild(document.createTextNode(movie.name));
   li.appendChild(deleteButton);
   movieList.appendChild(li);
-  textArea.value = "";
+  
 
+  
+ 
+  textArea.value="";
 
-  saveToLocalStorage();
 
 }
 
-function saveToLocalStorage() {
-
-  localStorage.clear();
-  var moviesLength = movieList.childNodes.length;
-
-  for (var i = 0; i < moviesLength; i++) {
-
-    var movie = movieList.childNodes[i].childNodes[0];
-
-    var key = "movie" + i;
-
-    localStorage.setItem(key, movie);
 
 
+function reloadMovies(){
+  
+  movieList.innerHTML="";
+
+  var size = localStorage.length;
+
+  for (var i = 0; i < size; i++) {
+
+
+
+    var key = localStorage.key(i);
+
+    var movie = new Movie();
+    movie.createMovieFromInfo(key,localStorage.getItem(key));
+    addMovie(movie);
 
   }
-
-  console.log(localStorage);
-
-
-
-
 }
 
 
-function deleteMovie(ev) {
-  var deleteButton = ev.target
-  var li = deleteButton.parentElement;
-  movieList.removeChild(li);
 
-  saveToLocalStorage();
+function createUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+     return v.toString(16);
+  });
 }
 
 
